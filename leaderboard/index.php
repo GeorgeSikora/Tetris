7fffad9c920001443e79f7df83b1e2f6c193a678
Jiří Sikora <jirisikoracz@email.cz>
2020-12-10 23:46:59 +0100

    small changes

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+	<title>Game over</title>
 	<?php header('Content-type: text/html; charset=utf-8'); ?>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="style.css">
--- leaderboard/score.php
+++ leaderboard/score.php
@@ -3,7 +3,7 @@
 
 	<p class="player-score">Skóre: <?php echo $_POST['score'];?></p>
 	
-	<form class="input-play-again" name="play-form" action="/tetris/index.php" method="post">
+	<form class="input-play-again" name="play-form" action="/tetris/" method="post">
 			<input type="hidden" id="name" name="player_name" placeholder="jméno nebo přezdívka" autocomplete="off" value="<?php echo $_POST['name'];?>"></input>
 			<button class="play-again-button" type="submit">Hrát znovu</button>
 	</form>
